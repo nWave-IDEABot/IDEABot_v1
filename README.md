5b8189df374be1ac72fca01b53eb5336800e9288
Gunalan S <34304620+s-gunalan@users.noreply.github.com>
2018-08-27 17:50:31 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# IDEABot_v1
+# nWave-Flask-Demo
+Flask Webservice (Python) which is deployed to Heroku
+https://nwave-ideabot-flask-webhook-p.herokuapp.com/webhook
---
+++ static/mainscript.js
@@ -0,0 +1,103 @@
+var local = {};
+local.avatar = "iqlogo.png";
+
+var remote = {};
+remote.avatar = "https://developers.viber.com/images/apps/apiai-icon.png";
+var SESSIONID="TESTINPUT-001";
+var accessToken = "3c44974b43934bbdb1fdc030b17df30e";
+var baseUrl = "https://api.api.ai/v1/";
+
+function formatTime(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}            
+
+function insertChat(who, text){
+    var control = "";
+    var date = formatTime(new Date());
+    
+     if (who == "local"){
+        
+         control = '<li style="width:80%;align:right">' +
+                        '<div class="msj-rta macro">' +
+                            '<div class="text text-r">' +
+                                '<p>'+text+'</p>' +
+                                '<p><small>'+date+'</small></p>' +
+                            '</div>' +                                                       
+                  '</li>';                   
+    }else{
+        control = '<li style="width:80%;align:right">' +
+                        '<div class="msj macro">' +
+                            '<div class="text text-l">' +
+                                '<p>'+ text +'</p>' +
+                                '<p><small>'+date+'</small></p>' +
+                            '</div>' +
+                        '</div>' +
+                    '</li>';   
+    }
+    $("#messages").append(control);
+    var objDiv = document.getElementById("messages");
+    objDiv.scrollTop = objDiv.scrollHeight;
+}
+
+$("#chat-panel").on('click',function(){
+    $(".innerframe").toggle();
+});
+
+function resetChat(){
+    $("#messages").empty();
+}
+
+$(".mytext").on("keyup", function(e){
+    if (e.which == 13){
+        var text = $(this).val();
+        if (text !== ""){
+            insertChat("local", text);              
+            $(this).val('');
+            queryBot(text)
+        }
+    }
+});
+
+resetChat();
+
+function queryBot(text) {
+            $.ajax({
+                type: "POST",
+                url: baseUrl + "query?v=20150910",
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                headers: {
+                    "Authorization": "Bearer " + accessToken
+                },
+                data: JSON.stringify({ query: text, lang: "en", sessionId: SESSIONID }),
+                success: function(data) {
+                    insertChat("remote",data.result.fulfillment.speech);
+                },
+                error: function() {
+                    insertChat("remote","Sorry My Bot has faced some issues! Please try again later");
+                }
+            });
+    }
+function createDynamicURL()
+{
+    //The variable to be returned
+    var URL;
+    URL="COG-EAS-IPM-nWAVE";
+    URL+=
+    function guid() {
+        function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
+    return URL;
+}
